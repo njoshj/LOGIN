555b1ac02348c48a62efa609335c69fcbcc74dbc
njoshj <47415494+njoshj@users.noreply.github.com>
2019-03-12 12:51:37 +0300

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+//initialize the session
+session_start();
+
+//unset all of the session variables
+$_SESSION = array();
+
+//destroy the session.
+session_destroy();
+
+//redirect to log in page
+header("location:login.php");
+exit;
+
+?>
